df7a1d7711c812926be403285fb34a666991bc90
my-how-to <alex.petrenco@gmail.com>
2026-06-02 09:55:29 +0300

    minor clean up

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Log In', async ({ page }) => {
+test('should login successfully with valid credentials', async ({ page }) => {
   await page.goto(process.env.BASE_URL!);
   await page.getByRole('textbox', { name: 'E-Mail-Adresse' }).fill(process.env.SITE_USER_AUDITOR!);
   await page.getByRole('textbox', { name: 'Passwort' }).fill(process.env.SITE_USER_AUDITOR_PASS!);
